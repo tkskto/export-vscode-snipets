64b437aff00f3a0ec79f75c8269286c2c54ffa3c
tkskto <tkskto@gmail.com>
2023-01-28 23:00:05 +0900

    feat: add html-node-parser options.

--- packages/@export-vscode-snippets/snippet-finder-html/types/index.d.ts
+++ packages/@export-vscode-snippets/snippet-finder-html/types/index.d.ts
@@ -1,2 +1,13 @@
-export function findSnippets(src: string, snippetSelector: SnippetSelector): Promise<Snippet[]>
-export const type: 'html'
+import {Options} from 'globby';
+
+export type HTMLFinderOption = {
+    'html-node-parser': Options
+};
+
+export function findSnippets(
+    src: string,
+    snippetSelector: SnippetSelector,
+    options: HTMLFinderOption
+): Promise<Snippet[]>;
+
+export const type: 'html';
